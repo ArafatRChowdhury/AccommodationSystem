1813c40fddc1e02f35a9d68ce8a8c424bcf765fe
Jaimepv1 <148768760+Jaimepv1@users.noreply.github.com>
2024-12-18 19:47:42 +0000

    Update CleaningStatus.java

--- CleaningStatus.java
+++ CleaningStatus.java
@@ -20,3 +20,22 @@ public enum CleaningStatus {
 
     } 
 }
+
+/*Code made by Jaime for the Change Cleaning Status Class
+public class CleaningStatus {
+    public enum cleaningStatus {
+        CLEAN,
+        MAINTENANCE,
+        DIRTY
+    }
+    public static void main(String []args){
+        cleaningStatus cleaningstatus = cleaningStatus.CLEAN;
+        switch(cleaningstatus){
+            case CLEAN -> System.out.println("The accommodation is clean");
+            case MAINTENANCE -> System.out.println("The accommodation is in maintenance");
+            case DIRTY -> System.out.println("The accommodation is dirty");
+        }
+    }
+      
+}
+*/
