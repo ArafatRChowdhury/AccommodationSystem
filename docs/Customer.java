c249f4b982495b74e5ff4958de3bfeb08d170065
Jaimepv1 <148768760+Jaimepv1@users.noreply.github.com>
2025-02-06 16:32:37 +0000

    Create Customer.java

---
+++ docs/Customer.java
@@ -0,0 +1,19 @@
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private String mobileNo;
+public Customer(String firstName, String lastName, String mobileNo){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.mobileNo = mobileNo;
+    }
+  public String getfirstName(){
+      return "Customer First Name";
+      }
+  public String getlastName(){
+      return "Customer Last Name";
+      }
+  public String getmobileNo(){
+      return "+44 7898232100";
+  }
+}
