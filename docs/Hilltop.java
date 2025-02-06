fd6a262121bab52451d6bb81f9d20d5b86f88517
Jaimepv1 <148768760+Jaimepv1@users.noreply.github.com>
2025-02-06 16:30:07 +0000

    Create Hilltop.java

---
+++ docs/Hilltop.java
@@ -0,0 +1,11 @@
+public class Hilltop extends Area {
+    public Hilltop(String areaName,String areaDescription,CleaningStatus.Status cleaningStatus){
+            super(areaName, areaDescription,cleaningStatus);
+            }
+    public String getareaName(){
+       return "Hilltop"; 
+    }
+    public String getareaDescription(){
+        return "Area Description in progress";
+    }
+}
