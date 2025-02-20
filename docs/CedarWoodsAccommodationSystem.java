a4340a1ddb49d91536e7788ce45d2dab08ba2ee1
ArafatRChowdhury <148768784+ArafatRChowdhury@users.noreply.github.com>
2025-02-20 17:14:21 +0000

    Create CedarWoodsAccommodationSystem.java

---
+++ docs/CedarWoodsAccommodationSystem.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.util.ArrayList;
+
+public class CedarWoodsAccommodationSystem extends Application {
+    
+    private ArrayList<Accommodation> accommodations;
+    
+    @Override
+    public void start(Stage primaryStage)
+            throws IOException
+    {
+        // Parent root = FXMLLoader.load(getClass().getResource("FXMLLogin.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("AccommodationSystem.fxml"));
+        
+        //Scene scene = new Scene(button, 200, 100);
+        primaryStage.setTitle("Cedar Woods Accommodation System");
+        Scene cedarWoodsAccommodationSystemScene = new Scene(loader.load());
+        
+        // FXMLGlampingSystemController controller = loader.getController();
+        // controller.setUWEAccommodationSystemGUI(uweAccommodationSystemGUI);
+        primaryStage.setScene(cedarWoodsAccommodationSystemScene);
+        primaryStage.show(); 
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
