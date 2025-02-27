8b8765724aa654a34d84971ed515c09bd858a213
ArafatRChowdhury <148768784+ArafatRChowdhury@users.noreply.github.com>
2025-02-27 17:20:30 +0000

    Update Woodland.java

--- docs/Woodland.java
+++ docs/Woodland.java
@@ -1,11 +1,11 @@
 public class Woodland extends Area {
-    public Woodland(String areaName,String areaDescription,CleaningStatus.Status cleaningStatus){
-            super(areaName, areaDescription,cleaningStatus);
+    public Woodland(String areaName,String areaDescription){
+            super(areaName, areaDescription);
             }
     public String getareaName(){
        return "Woodland"; 
     }
     public String getareaDescription(){
-        return "Area Description in progress";
+        return areaDescription;
     }
 }
