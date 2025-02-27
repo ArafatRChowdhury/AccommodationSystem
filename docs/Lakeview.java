9fffcf9f5654d119daa848e08f406d5342444bde
ArafatRChowdhury <148768784+ArafatRChowdhury@users.noreply.github.com>
2025-02-27 17:21:46 +0000

    Update Lakeview.java

--- docs/Lakeview.java
+++ docs/Lakeview.java
@@ -1,11 +1,11 @@
 public class Lakeview extends Area {
-    public Lakeview(String areaName,String areaDescription,CleaningStatus.Status cleaningStatus){
-            super(areaName, areaDescription,cleaningStatus);
+    public Lakeview(String areaName,String areaDescription){
+            super(areaName, areaDescription);
             }
     public String getareaName(){
        return "Lakeview"; 
     }
     public String getareaDescription(){
-        return "Area Description in progress";
+        return areaDescription;
     }
 }
