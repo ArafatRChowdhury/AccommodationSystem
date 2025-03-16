0dbd92879af9e2a8410fedb73b3ab734cdc3347e
Htet-Aungg <159455430+Htet-Aungg@users.noreply.github.com>
2025-03-16 16:01:36 +0000

    Update Accommodation.java

--- docs/Accommodation.java
+++ docs/Accommodation.java
@@ -1,81 +1,136 @@
-package groupproject.accommodationsystem;
+package cedarwoodsaccommodationsystem.model;
 
 public class Accommodation {
-  private String accommodationName;
-  private String accommodationDescription;
-  private Integer guests;
-  private int costperNight;
-  private boolean occupancy;
-  private boolean availability;
-  private CleaningStatus.Status cleaningStatus;
-  private Boolean breakfast;
-  private String area;
-    
-public Accommodation(String accommodationName, String accommodationDescription, int guests, int costperNight, boolean occupancy,boolean availability,CleaningStatus.Status cleaningStatus,boolean breakfast){
-    this.accommodationName=accommodationName;
-    this.accommodationDescription=accommodationDescription;
-    this.guests = guests;
-    this.costperNight = costperNight;
-    this.occupancy = occupancy;
-    this.availability = availability;
-    this.cleaningStatus = cleaningStatus;
-    this.breakfast = breakfast;
-    this.area = area;
-}
-    public String getAccommodationName(){
-        return accommodationName;
-        }
-    public String getAccommodationDescription(){
-        return accommodationDescription;
-        }
-    public Integer getGuests(){
-        return guests;
-        }
-    public int getCost(){
-        return costperNight;
-        }
-    public boolean getOccupancy(){
-        return occupancy;
-        }
-    public boolean getAvailability(){
-        return availability;
-        }
-    public Boolean getBreakfast(){
-        return breakfast;
-        }
-    public CleaningStatus.Status getCleaningStatus(){
-           return cleaningStatus;
-        }
-    public int setGuests(int guests){
-        return this.guests = guests;
-        //Make a loop to limit the amount of guests for each accommodation
-        }
-    public boolean setOccupancy(boolean occupancy){
-        return this.occupancy = occupancy;
-        }
-    public boolean setAvailability(boolean availability){
-        return this.availability = availability;
-        }
-    //Make a set cleaning status object
-    public void setCleaningStatus(CleaningStatus.Status cleaningStatus) {
-    this.cleaningStatus = cleaningStatus;
-    System.out.println("Cleaning status updated to: " + this.cleaningStatus);
-    }
-    public Boolean setBreakfast(boolean breakfast){
-        return this.breakfast = breakfast;
-        }
-    public class checkIn extends RentalAgreement{
-        public checkIn(boolean breakfast, int checkinDate, int numberofGuests, int numberofNights){
-            super(breakfast,checkinDate, numberofGuests,numberofNights);
-        }
-    }
-   public boolean needsCleaning() {
-    return cleaningStatus != CleaningStatus.Status.CLEAN;
+    private String accommodationName;
+    private String accommodationDescription;
+    private int guests;
+    private int costperNight;
+    private boolean occupancy;
+    private boolean availability;
+    private CleaningStatus.Status cleaningStatus;
+    private boolean breakfast;
+    private String area;
+    private Customer customer;
+    private RentalAgreement rentalAgreement;
+
+    public Accommodation(String accommodationName, String accommodationDescription, int guests, int costperNight, boolean occupancy, boolean availability, CleaningStatus.Status cleaningStatus, boolean breakfast) {
+        this.accommodationName = accommodationName;
+        this.accommodationDescription = accommodationDescription;
+        this.guests = guests;
+        this.costperNight = costperNight;
+        this.occupancy = occupancy;
+        this.availability = availability;
+        this.cleaningStatus = cleaningStatus;
+        this.breakfast = breakfast;
+        this.area = ""; // Initialize area to an empty string or pass it as a parameter
+        this.customer = null; // Initially, no customer is assigned
+        this.rentalAgreement = null; // Initially, no rental agreement is assigned
     }
 
-    public String area(){
+    // Getters
+    public String getAccommodationName() {
+        return accommodationName;
+    }
+
+    public String getAccommodationDescription() {
+        return accommodationDescription;
+    }
+
+    public int getGuests() {
+        return guests;
+    }
+
+    public int getCost() {
+        return costperNight;
+    }
+
+    public boolean getOccupancy() {
+        return occupancy;
+    }
+
+    public boolean getAvailability() {
+        return availability;
+    }
+
+    public boolean getBreakfast() {
+        return breakfast;
+    }
+
+    public CleaningStatus.Status getCleaningStatus() {
+        return cleaningStatus;
+    }
+
+    public String getArea() {
         return area;
     }
-    //Make a check out class to check out the customer from the system
-    
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public RentalAgreement getRentalAgreement() {
+        return rentalAgreement;
+    }
+
+    // Setters
+     public int setGuests(int guests){
+        if(guests >= 1 && guests <= 4){
+            this.guests = guests    ; // update the guests field
+        }
+        else{
+            System.out.println("Invalid number of guests. Must be between 1 and 4.");
+        }
+        return 0;
+     }
+
+    public void setOccupancy(boolean occupancy) {
+        this.occupancy = occupancy;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+
+    public void setCleaningStatus(CleaningStatus.Status cleaningStatus) {
+        this.cleaningStatus = cleaningStatus;
+        System.out.println("Cleaning status updated to: " + this.cleaningStatus);
+    }
+
+    public void setBreakfast(boolean breakfast) {
+        this.breakfast = breakfast;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public void setRentalAgreement(RentalAgreement rentalAgreement) {
+        this.rentalAgreement = rentalAgreement;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    // Additional methods
+    public boolean needsCleaning() {
+        return cleaningStatus != CleaningStatus.Status.CLEAN;
+    }
+
+    @Override
+    public String toString() {
+        return "Accommodation{" +
+                "accommodationName='" + accommodationName + '\'' +
+                ", accommodationDescription='" + accommodationDescription + '\'' +
+                ", guests=" + guests +
+                ", costperNight=" + costperNight +
+                ", occupancy=" + occupancy +
+                ", availability=" + availability +
+                ", cleaningStatus=" + cleaningStatus +
+                ", breakfast=" + breakfast +
+                ", area='" + area + '\'' +
+                ", customer=" + (customer != null ? customer.getFirstName() + " " + customer.getLastName() : "No customer") +
+                ", rentalAgreement=" + (rentalAgreement != null ? rentalAgreement.toString() : "No rental agreement") +
+                '}';
+    }
 }
